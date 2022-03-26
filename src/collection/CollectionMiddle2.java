86c248414e807861887e967ef74533d730063c3a
AlinaLuzanova <alinaluzanova@gmali.com>
2022-03-26 14:54:17 +0300

    Initial commit1111

--- src/collection/CollectionMiddle2.java
+++ src/collection/CollectionMiddle2.java
@@ -8,24 +8,24 @@ import java.util.*;
 
 public class CollectionMiddle2 {
     public static void main(String[] args) throws IOException {
-        File file1 = new File("/Users/alina/IdeaProjects/HOMEWORK/src/file1");
-        File file2 = new File("/Users/alina/IdeaProjects/HOMEWORK/src/file2");
+        File file3 = new File("/Users/alina/IdeaProjects/HOMEWORK/src/file3");
+        File file4 = new File("/Users/alina/IdeaProjects/HOMEWORK/src/file4");
 
-        FileReader fr = new FileReader(file1);
-        PrintWriter pw = new PrintWriter(file2, "UTF-8");
+        FileReader fr = new FileReader(file3);
+        PrintWriter pw = new PrintWriter(file4, "UTF-8");
         Scanner sc = new Scanner(fr);
 
-        LinkedHashSet<Integer> hashSet = new LinkedHashSet<>();
+        LinkedHashSet<String> hashSet = new LinkedHashSet<>();
         while (sc.hasNext()) {
-            int a = sc.nextInt();
+            String a = sc.nextLine();
             hashSet.add(a);
         }
         fr.close();
 
         Object[] objects = hashSet.toArray();
-        int[] array = new int[objects.length];
+        String [] array = new String[objects.length];
         for (int i = 0; i < objects.length; i++) {
-            array[i] = (int) objects[i];
+            array[i] = (String) objects[i];
             pw.println(array[i]);
         }
         pw.close();
